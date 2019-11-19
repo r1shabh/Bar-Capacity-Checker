5c6ff3c02b539a8375d0bbe1a63f14328cbc9705
RIshabh Anand <ranubis01@gmail.com>
2019-11-19 15:56:53 -0500

    Initial Commit

---
+++ Bar Capacity Checker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bar Capacity Checker
+//
+//  Created by Rishabh Anand on 11/19/19.
+//  Copyright © 2019 Rishabh Anand. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
